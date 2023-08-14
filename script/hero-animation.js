dfddb5efa1f3e86b7b56aaec6b71d4e808e1336f
akademuk <akademuk24@gmail.com>
2023-08-14 10:14:21 +0300

    sss

--- script/hero-animation.js
+++ script/hero-animation.js
@@ -4,17 +4,17 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         const block1 = document.querySelector('.hero-right-box-item1');
         block1.style.opacity = '1';
-    }, 1000); // 1000 миллисекунд = 1 секунда
+    }, 2200); // 1000 миллисекунд = 1 секунда
 
     // Появление второго блока через 2 секунды
     setTimeout(function () {
         const block2 = document.querySelector('.hero-right-box-item2');
         block2.style.opacity = '1';
-    }, 2000); // 2000 миллисекунд = 2 секунды
+    }, 2700); // 2000 миллисекунд = 2 секунды
 
     // Появление третьего блока через 3 секунды
     setTimeout(function () {
         const block3 = document.querySelector('.hero-right-box-item3');
         block3.style.opacity = '1';
-    }, 3000); // 3000 миллисекунд = 3 секунды
+    }, 3200); // 3000 миллисекунд = 3 секунды
 });
