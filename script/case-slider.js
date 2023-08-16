09100507280204042f7d45450ba91354ccfdf7ff
akademuk <akademuk24@gmail.com>
2023-08-16 16:57:42 +0300

    ss

--- script/case-slider.js
+++ script/case-slider.js
@@ -1,14 +1,40 @@
 // Инициализация Swiper Slider только на мобильных устройствах
 if (window.matchMedia("(max-width: 1200px)").matches) {
-  const swiper = new Swiper('.case-box', {
-    spaceBetween: 10,
-    slidesPerView: 'auto',
-    slideToClickedSlide: true,
+  var $swiperSelector = $('.case-box');
 
-    pagination: {
-      el: '.case-pagination',
-      clickable: true,
-    },
-});
+  $swiperSelector.each(function(index) {
+      var $this = $(this);
+      $this.addClass('swiper-slider-' + index);
+      
+      var dragSize = $this.data('drag-size') ? $this.data('drag-size') : 50;
+      var freeMode = $this.data('free-mode') ? $this.data('free-mode') : false;
+      var loop = $this.data('loop') ? $this.data('loop') : false;
+      var slidesDesktop = $this.data('slides-desktop') ? $this.data('slides-desktop') : 4;
+      var slidesTablet = $this.data('slides-tablet') ? $this.data('slides-tablet') : 4;
+      var slidesMobile = $this.data('slides-mobile') ? $this.data('slides-mobile') : 1.2;
+      var spaceBetween = $this.data('space-between') ? $this.data('space-between'): 20;
+  
+      var swiper = new Swiper('.swiper-slider-' + index, {
+        direction: 'horizontal',
+        loop: loop,
+        freeMode: freeMode,
+        spaceBetween: spaceBetween,
+        breakpoints: {
+          1920: {
+            slidesPerView: slidesDesktop
+          },
+          992: {
+            slidesPerView: slidesTablet
+          },
+          320: {
+             slidesPerView: slidesMobile
+          }
+        },
+        pagination: {
+          el: '.case-pagination',
+          clickable: true,
+        },
+     });
+  });
 }
   
--- script/certificates-slider.js
+++ script/certificates-slider.js
@@ -1,13 +1,13 @@
   // Инициализация Swiper Slider только на мобильных устройствах
 if (window.matchMedia("(max-width: 1200px)").matches) {
   const swiper1 = new Swiper('.certificates-container', {
-    slidesPerView: 1, 
-    spaceBetween: 16,
-    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
       pagination: {
         el: '.certificates-pagination',
         clickable: true,
       },
   });
+  
 }
 
