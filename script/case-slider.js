e6b2014f2dbe9b858055815e7411b9b966a3ab28
akademuk <akademuk24@gmail.com>
2023-08-16 16:13:30 +0300

    sss

--- script/case-slider.js
+++ script/case-slider.js
@@ -1,9 +1,9 @@
 // Инициализация Swiper Slider только на мобильных устройствах
 if (window.matchMedia("(max-width: 1200px)").matches) {
   const swiper = new Swiper('.case-box', {
-    slidesPerView: 'auto', // Автоматически определит количество слайдов на экране
-    spaceBetween: 16, // Расстояние между слайдами (можете настроить по своему вкусу)
-    loop: false, // Отключение бесконечной петли
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    slideToClickedSlide: true,
 
     pagination: {
       el: '.case-pagination',
