55c47f97cb2f6a699cd7776ea1bafb4dc6e7ea46
akademuk <akademuk24@gmail.com>
2023-08-16 08:46:53 +0300

    sss

--- script/certificates-slider.js
+++ script/certificates-slider.js
@@ -1,9 +1,9 @@
   // Инициализация Swiper Slider только на мобильных устройствах
 if (window.matchMedia("(max-width: 1200px)").matches) {
   const swiper1 = new Swiper('.certificates-container', {
-    slidesPerView: 'auto', // Автоматически определит количество слайдов на экране
-    spaceBetween: -9, // Расстояние между слайдами (можете настроить по своему вкусу)
-    loop: true, // Отключение бесконечной петли
+    slidesPerView: 'auto', 
+    spaceBetween: 8,
+    loop: false,
       pagination: {
         el: '.certificates-pagination',
         clickable: true,
